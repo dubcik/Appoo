edf8230bfc17d83b8453a5785104c61a1da6f053
Andrei Dubovoi <andrei_dubovoi@mail.ru>
2015-02-09 09:59:06 +0200

    *.cpp

--- Лаб1 APPOO/Исходный код.cpp
+++ Лаб1 APPOO/Исходный код.cpp
@@ -13,15 +13,24 @@ int main(){
 	
 	while (1){
 		cout << " Enter number of operation:\n";
+<<<<<<< HEAD
 		cout << " 1.Enter vector.\n" " 2.Show vector number 1.\n" " 3.Delet element of vector.\n";
 		cout << " 4.Exit. \n" " 5.Delet Vector.\n" " 6.Add to vector 1-n + vector 2.\n" " 7.Show vector number 1 and number 2. \n";
+=======
+		cout << " 1.Enter vector.\n" " 2.Show vector №1.\n" " 3.Delet element of vector.\n";
+		cout << " 4.Exit. \n" " 5.Delet Vector.\n" " 6.Add to vector1-n + vector2.\n" " 7.Show vector №1 and №2. \n";
+>>>>>>> f9c31e11993b82984f5b04ab71bf19a517349241
 
 		int in;
 		string input;
 		cin >> in;
 		switch (in)
 		{
+<<<<<<< HEAD
 		case(1) ://Add to Vector1
+=======
+		case(1) ://ввод символов в вектор
+>>>>>>> f9c31e11993b82984f5b04ab71bf19a517349241
 		{
 			cout << " Enter number:   *(for exit whrite> exit )\n";
 			while (cin >> input) {
@@ -31,7 +40,11 @@ int main(){
 			}}
 
 				 break;
+<<<<<<< HEAD
 		case(2) ://Show Vector1
+=======
+		case(2) ://вывод на экран 1 вектора
+>>>>>>> f9c31e11993b82984f5b04ab71bf19a517349241
 		{
 			cout << " Vector number:   \n";
 			for (vector<string>::const_iterator iter = v.begin(); iter != v.end(); ++iter) {
@@ -39,7 +52,11 @@ int main(){
 																							}
 		}
 				 break;
+<<<<<<< HEAD
 		case(3) ://Delete element
+=======
+		case(3) ://удаление элемента
+>>>>>>> f9c31e11993b82984f5b04ab71bf19a517349241
 		{
 
 			string m;
@@ -53,12 +70,20 @@ int main(){
 							}
 		}
 				 break;
+<<<<<<< HEAD
 		case(4) ://Exit
+=======
+		case(4) ://Выход
+>>>>>>> f9c31e11993b82984f5b04ab71bf19a517349241
 		{ v.clear();
 			return 0;
 		}
 				 break;
+<<<<<<< HEAD
 		case(5) ://Clear Vector 1
+=======
+		case(5) ://очистка контейнера вектора
+>>>>>>> f9c31e11993b82984f5b04ab71bf19a517349241
 		{
 			v.clear();
 		}
@@ -82,7 +107,11 @@ int main(){
 			
 		}
 				   break;
+<<<<<<< HEAD
 		case(7)://Show Vector1 and Vector2 
+=======
+		case(7)://вывод на экран 1 вектора
+>>>>>>> f9c31e11993b82984f5b04ab71bf19a517349241
 		{
 			cout << " Vector 1 number:   \n";
 			for (vector<string>::const_iterator iter = v.begin(); iter != v.end(); ++iter) {
